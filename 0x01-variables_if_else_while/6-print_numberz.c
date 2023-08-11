ef399197251d26bd718a5450f195a6a745f4b30d
oluwakuty <oyindamoanu@gmail.com>
2023-08-11 11:02:27 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always eachtime 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	x = '0';
+	while
+		(x < 10) {
+			putchar(x + '0');
+			x++;
+		}
+	putchar('\n');
+	return (0);
+}
