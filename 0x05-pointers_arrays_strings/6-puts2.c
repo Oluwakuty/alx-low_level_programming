0a7d67a3363668bd8970dcfb12348d14aae55fa3
oluwakuty <oyindamoanu@gmail.com>
2023-08-21 08:12:46 -0700

    6-puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * void puts2 - prints character of a string
+ * @str: variable string
+ * Return: Always return 0
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		printf("%c", str[i]);
+		i += 2;
+	}
+	printf("\n");
+}
