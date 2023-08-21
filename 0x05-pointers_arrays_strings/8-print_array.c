9f38fe260ff78ca98d37472189e8c69dfc536cfe
oluwakuty <oyindamoanu@gmail.com>
2023-08-21 09:18:11 -0700

    8-print_array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/** print_array - print array number
+ * @a: parameters to be printed
+ * @n: parameters to be printed
+ * Return: Alwas return success 0
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+			printf(", ");
+	}
+	printf("\n");
+}
