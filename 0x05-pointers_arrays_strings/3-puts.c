02349fe7b2191488c80834b07fa7272badff5a2d
oluwakuty <oyindamoanu@gmail.com>
2023-08-21 03:05:07 -0700

    3-puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by new line
+ * @str: Variable characters
+ * Return: Always everytime return 0
+ */
+
+void _puts(char *str)
+{
+	printf("%s\n", str);
+	
+}
