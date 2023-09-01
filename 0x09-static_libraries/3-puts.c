029aadc6b80333a8bace2779c0e4d06d1e2783be
oluwakuty <oyindamoanu@gmail.com>
2023-09-01 13:14:53 -0700

    static library

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0');
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
