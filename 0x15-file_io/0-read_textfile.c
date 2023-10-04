e1e7b66dad123fef1208d03d26baaa9333dc1777
oluwakuty <oyindamoanu@gmail.com>
2023-10-03 22:40:31 -0700

    task zero

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,25 +10,20 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fild = open(filename, O_RDONLY);
-	char *buff = malloc(letters);
-	ssize_t bytes_read = read(fild, buff, letters);
-	ssize_t bytes_written = (bytes_read > 0) ?
-		write(STDOUT_FILENO, buff, bytes_read) : -1;
+	char *buff;
+	ssize_t fild;
+	ssize_t m;
+	ssize_t y;
 
-	if (filename == NULL)
-		return (0);
-
+	fild = open(filename, O_RDONLY);
 	if (fild == -1)
 		return (0);
 
-	if (!buff)
-	{
-		close(fild);
-		return (0);
-	}
+	buff = malloc(sizeof(char) * letters);
+	y = read(fild, buff, letters);
+	m = write(STDOUT_FILENO, buff, y);
 	free(buff);
 	close(fild);
 
-	return (bytes_written);
+	return (m);
 }
